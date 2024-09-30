43276e35f140c2b979c925e298aae33474c54e81
Norman Chen <bird.dream.norman@gmail.com>
2024-09-30 14:27:09 +0800

    stash

--- src/components/TopList.tsx
+++ src/components/TopList.tsx
@@ -1,34 +1,35 @@
-import { useState } from "react";
-import { useWriteContract, useAccount, useReadContract } from "wagmi";
-
+import { useReadContract } from "wagmi";
 import { wagmiContractConfig } from "./contracts";
 
 export function TopList() {
-  //   const { data, error, isPending, isError, writeContract } = useWriteContract();
-  //   const { addresses } = useAccount();
+  //   const { data, isRefetching, refetch, isLoading, isSuccess } = useReadContract(
+  //     {
+  //       ...wagmiContractConfig,
+  //       functionName: "getLeaderboard",
+  //       args: [],
+  //     }
+  //   );
 
-  const { data, isRefetching, refetch, isLoading, isSuccess } = useReadContract(
-    {
-      ...wagmiContractConfig,
-      functionName: "getLeaderboard",
-    }
-  );
+  const { data, error, isPending } = useReadContract({
+    ...wagmiContractConfig,
+    functionName: "getLeaderboard",
+  });
 
-  console.log("--------data");
+  console.log("=====");
   console.log(data);
-  console.log(isLoading);
-  console.log(isSuccess);
+  console.log(isPending);
+  console.log(error);
 
   return (
     <div>
       <button
-        onClick={async () => refetch()}
+        // onClick={async () => refetch()}
         className="border-2 border-gray-300 rounded-md px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
         click me! Top List
       </button>
       {/* <div>{data}</div> */}
-      <div>{isRefetching}</div>
+      {/* <div>{isRefetching}</div> */}
       {/* <div>
         游戏值{score}-{bestScore}
       </div> */}
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,15 +1,15 @@
 import { http, cookieStorage, createConfig, createStorage } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { mainnet, sepolia, linea, lineaSepolia } from "wagmi/chains";
 import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
 
 export function getConfig() {
   return createConfig({
-    chains: [mainnet, sepolia],
+    chains: [mainnet, sepolia, linea, lineaSepolia],
     connectors: [
       injected(),
       coinbaseWallet(),
       // walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID }),
-      walletConnect({ projectId: '78ce7ccba8e8e93ccd5273baa0314097' }),
+      walletConnect({ projectId: "78ce7ccba8e8e93ccd5273baa0314097" }),
     ],
     storage: createStorage({
       storage: cookieStorage,
@@ -18,6 +18,8 @@ export function getConfig() {
     transports: {
       [mainnet.id]: http(),
       [sepolia.id]: http(),
+      [linea.id]: http(),
+      [lineaSepolia.id]: http(),
     },
   });
 }
